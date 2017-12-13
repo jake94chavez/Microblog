6898002197eb806f9d6d5ff08c81c06f59252963
Jake Chavez <jake94chavez@gmail.com>
2017-12-12 21:02:59 -0600

    Commit 2 - Posts now add themself to the page on button click

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const allPosts = [];
 
-// const makeAPost = () => {
-// 	let blurb = document.createElement('li');
-// 	let field = 4;
-// 	blurb.appendChild(field);
-// 	document.getElementById('feed').appendChild(blurb);
-// }
-
-// When the button is clicked, run the function to add post to the allPosts array and reset the value to null
+//When the button is clicked, run the function to add post to the allPosts array and reset the value to null
 document.getElementById('button').addEventListener('click', postToArray = () => {
 	let toPost = document.getElementById('submission').value
 	allPosts.unshift(toPost);
 	document.getElementById('submission').value = null;
+	makeAPost();
 });
 
 // No clue why but allPosts array starts with allPosts[0] = ""
 // This is here to get rid of that
 if (allPosts.last === "") {
 	allPosts.pop();
+}
+
+const makeAPost = () => {
+	let blurb = document.createElement('LI');
+	let postText = allPosts[0]
+	blurb.append(postText);
+	document.getElementById('feed').appendChild(blurb);
 }
