4c9500c347f540edefbe6a2e5e7c6038897741c8
Jake Chavez <jake94chavez@gmail.com>
2017-12-14 20:39:11 -0600

    Posts now show up newest on top. Still needs CSS.

--- app.js
+++ app.js
@@ -3,14 +3,37 @@ const allPosts = [];
 //When the button is clicked, run the function to add post to the allPosts array and reset the value to null
 document.getElementById('button').addEventListener('click', postToArray = () => {
 	let toPost = document.getElementById('submission').value
+	if (document.getElementById('submission').value == '') {
+		alert('There is nothing to post!')
+	} else {
 	allPosts.unshift(toPost);
 	document.getElementById('submission').value = null;
 	makeAPost();
+	}
 });
 
+document.getElementById('submission').addEventListener('keyup', (event) => {
+	if (event.which === 13) {
+	let toPost = document.getElementById('submission').value
+	if (document.getElementById('submission').value == '') {
+		alert('There is nothing to post!')
+	} else {
+	allPosts.unshift(toPost);
+	document.getElementById('submission').value = null;
+	makeAPost();
+	}
+}
+});
+
+
 const makeAPost = () => {
-	let blurb = document.createElement('LI');
+	let history = document.getElementById('feed');
+	let blurb = document.createElement('li');
 	let postText = allPosts[0]
 	blurb.append(postText);
-	document.getElementById('feed').appendChild(blurb);
+	if (allPosts.length == 1) {
+	history.appendChild(blurb);	
+	} else {
+		history.insertBefore(blurb, history.childNodes[0]);	
+	}
 }
